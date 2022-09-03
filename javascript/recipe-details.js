d636cfd80db046c2cdd642918bcc0e117a060890
Mariano Guzmán Rodríguez <guzmanmariano28@gmail.com>
2022-09-03 01:10:20 -0500

    Añadir comentarios sobre funcionamiento de cada elemento que permite desplegar los detalles de la receta

--- javascript/recipe-details.js
+++ javascript/recipe-details.js
@@ -1,21 +1,27 @@
+// Add reference that will display a random button
 const randomButton = document.querySelector('.button-random')
 
+// Add an event listener to get a random recipe when button click
 randomButton.addEventListener('click', () => {
   getRecipeDetails(event, 'random')
 })
 
+// Add an event listener to get the recipe by id
 results.addEventListener('click', () => {
   getRecipeDetails(event, 'lookup')
 })
 
+// Function to get the information to display in the modals
 async function getRecipeDetails(event, apimethod) {
   try {
+    // Get reference to DOM elements
     const titleSection = document.querySelector('.recipe-title')
     const ingredientsSection = document.querySelector('.recipe-ingredients')
     const measuresSection = document.querySelector('.recipe-measures')
     const recipeDescriptionContent = document.querySelector('.recipe-description-content')
     const recipeImage = document.querySelector('.recipe-image')
 
+    // Remove elements that already existed to avoid overlapping
     if (ingredientsSection.children) {
       const ingredientsChildren = Array.from(ingredientsSection.children)
       ingredientsChildren.forEach(item => item.remove())
@@ -26,10 +32,12 @@ async function getRecipeDetails(event, apimethod) {
       measuresChildren.forEach(item => item.remove())
     }
 
+    // Avoid image overlapping
     if (recipeImage) {
       recipeImage.src = ''
     }
 
+    // Obtain the ingredient and data for each type of API method
     let ingredient
     let data
 
@@ -40,6 +48,7 @@ async function getRecipeDetails(event, apimethod) {
       data = await getDataRandom()
     }
 
+    // Headers creation for each item
     const measuresHeader = document.createElement('h2')
     const ingredientsHeader = document.createElement('h2')
 
@@ -52,7 +61,8 @@ async function getRecipeDetails(event, apimethod) {
     titleSection.textContent = data[0]['strMeal']
     recipeDescriptionContent.textContent = data[0]['strInstructions']
     recipeImage.src = data[0]['strMealThumb']
-    
+   
+    // Obtain the keys that include the measures and ingredients so the DOM elements can be created
     const keys = Object.keys(data[0])
   
     const measureKeys = keys.filter(key => key.includes('strMeasure'))
